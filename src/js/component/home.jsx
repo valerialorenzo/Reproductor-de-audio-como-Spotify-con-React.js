d184b678dcfc4fdea4fe0e9df9fdb561e27839d8
valerialorenzo <valerialorenzo9@gmail.com>
2023-01-12 19:34:53 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,128 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-//include images into your bundle
+// include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [playList, setplayList] = useState([]);
+  const reproductor = useRef();
+  let playButton = useRef();
+  let pauseButton = useRef();
+  let [posicionMusica, setPosicionMusica] = useState(0);
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/sound/songs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return setplayList(data);
+      });
+  }, []);
+
+  function seleccionarMusica(url) {
+    reproductor.current.src = `https://assets.breatheco.de/apis/sound/${url}`;
+
+    // reproductor.current.play();
+	play();
+  }
+
+  function cambiarIcono() {
+    if (reproductor.current.pause) {
+		reproductor.current.play();
+      setvalorIcono("fafa-pause");
+    } else {
+		reproductor.current;
+    }
+  }
+
+  function atras() {
+	if (posicionMusica >0) 
+   { setPosicionMusica(posicionMusica--);}
+	const url = playList[posicionMusica].url;
+    reproductor.current.src = `https://assets.breatheco.de/apis/sound/${url}`;
+    // reproductor.current.play();
+	play()
+  }
+  function adelante() {
+    setPosicionMusica(posicionMusica++);
+	const url = playList[posicionMusica].url;
+    reproductor.current.src =  `https://assets.breatheco.de/apis/sound/${url}`;
+    // reproductor.current.play();
+play()  
+}
+
+
+  const play = () => {
+    reproductor.current.play();
+	playButton.current.style.display='none';
+	pauseButton.current.style.display = 'block';
+  }
+
+  const pause = () => {
+    reproductor.current.pause();
+	playButton.current.style.display='block';
+	pauseButton.current.style.display = 'none';
+  }
+  return (
+    <div className="container-text-center">
+      <ul className="list-group group-flash mb-3">
+        {playList.map((song) => (
+          <button
+            className="btn btn-dark text-start rounded-0"
+            type="button"
+            onClick={() => seleccionarMusica(song.url)}
+            key={song.id}
+          >
+            {song.id}
+            {song.name}-
+          </button>
+        ))}{" "}
+      </ul>
+
+      <div className="d-flex justify-content-center bg-secondary fixed-bottom">
+        <button onClick={atras}>
+          <i
+            className="fa fa-backward mx-2"
+            style={{
+              widgh: "50%",
+              height: "25%",
+            }}
+          ></i>
+        </button>
+        <button onClick={play} ref={playButton}>
+          <i
+            className="fa fa-play rounded"
+            style={{
+              widgh: "50%",
+              height: "25%",
+            }}
+          ></i>
+        </button>
+		<button onClick={pause} ref={pauseButton} style={{display:'none'}}>
+          <i
+            className="fa fa-pause"
+            style={{
+              widgh: "50%",
+              height: "25%"
+            }}
+          ></i>
+        </button>
+        <audio id="reproductor" ref={reproductor}></audio>
+        <button onClick={adelante}>
+          <i
+            className=" fa fa-forward mx-2"
+            style={{
+              widgh: "50%",
+              height: "25%",
+            }}
+          ></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
