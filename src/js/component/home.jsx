c251903dee43a73168800140fd736755a9fc3fc3
valerialorenzo <valerialorenzo9@gmail.com>
2023-01-13 15:18:53 +0000

    repetir cancion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
   let playButton = useRef();
   let pauseButton = useRef();
   let [posicionMusica, setPosicionMusica] = useState(0);
+  let [repetir, setRepetir] = useState(false);
 
   useEffect(() => {
     fetch("https://assets.breatheco.de/apis/sound/songs")
@@ -54,6 +55,10 @@ const Home = () => {
 play()  
 }
 
+const repetirMusica = () => {
+  setRepetir(!repetir);
+  //reproductor.current.loop = repetir;
+} 
 
 const subirVolumen = () => {
   reproductor.current.volume += 0.10;
@@ -119,7 +124,7 @@ const bajarVolumen = () => {
             }}
           ></i>
         </button>
-        <audio id="reproductor" ref={reproductor}></audio>
+        <audio id="reproductor" ref={reproductor} loop={repetir}></audio>
         <button onClick={adelante}>
           <i
             className=" fa fa-forward mx-2"
@@ -135,9 +140,17 @@ const bajarVolumen = () => {
           
         </button>
         <button className="fas fa-volume-up" onClick={subirVolumen}>
-
           
 </button>
+
+
+<button className="" onClick={repetirMusica}> 
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-repeat" viewBox="0 0 16 16">
+  <path stroke={repetir ? 'red' : 'black'} stroke-width="1.5" d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192Zm3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z"/>
+</svg>
+</button>
+
 
       </div>
     </div>
